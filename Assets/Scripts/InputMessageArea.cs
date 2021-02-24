5192a07da0e3b32bff42446281539529671da338
Marat Yakushev <yakushev1396@gmail.com>
2021-02-25 01:35:19 +0400

    UI and some scripts. Added TextMeshPro.

---
+++ Assets/Scripts/InputMessageArea.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputMessageArea : MonoBehaviour
+{
+    [SerializeField] TMP_InputField inputField;
+    [SerializeField] Button sendMessageButton;
+
+    public event Action<string> OnSendImageClick = t => { };
+
+    private void Awake()
+    {
+        sendMessageButton.onClick.AddListener(OnSendMessageClick);
+    }
+
+    private void OnSendMessageClick()
+    {
+        var text = inputField.text;
+        inputField.text = string.Empty;
+        OnSendImageClick(text);
+    }
+}
---
+++ Assets/Scripts/MessagesContentView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessagesContentView : MonoBehaviour
+{
+    [SerializeField] MessageView messageViewPrefab;
+    [SerializeField] RectTransform scrollContent;
+    [SerializeField] InputMessageArea inputArea;
+
+    private void Awake()
+    {
+        inputArea.OnSendImageClick += OnMessageSended;
+    }
+
+    private void OnDestroy()
+    {
+        inputArea.OnSendImageClick -= OnMessageSended;
+    }
+
+    private void OnMessageSended(string text)
+    {
+        var mv = Instantiate(messageViewPrefab);
+        mv.SetText(text);
+        mv.transform.SetParent(scrollContent.transform);
+    }
+}
